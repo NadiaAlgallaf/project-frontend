67461349630930286bacf10e7e4d4a390c47b682
Nadia Algallaf <nadiaalgallaf164@gmail.com>
2026-08-08 17:48:16 +0300

    add jobs pages and job service

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
-import { useState } from "react";
-import { Route, Routes } from "react-router";
-import Navbar from "./components/Navbar";
-import SignupPage from "./pages/SignupPage";
-import Homepage from "./pages/Homepage";
-import SignInPage from "./pages/SigninPage";
-import Dashboard from "./pages/Dashboard";
-import { useEffect } from "react";
-import { getCurrentUser, logout } from "./services/authService";
-import ProtectedRoute from "./components/ProtectedRoute";
-import { useAuth } from "./context/AuthContext";
+import { useState } from 'react'
+import { Route, Routes } from 'react-router'
+import Navbar from './components/Navbar'
+import SignupPage from './pages/SignupPage'
+import Homepage from './pages/Homepage'
+import SignInPage from './pages/SigninPage'
+import Dashboard from './pages/Dashboard'
+import { useEffect } from 'react'
+import { getCurrentUser, logout } from './services/authService'
+import ProtectedRoute from './components/ProtectedRoute'
+import { useAuth } from './context/AuthContext'
+import AllJobs from './pages/AllJobs'
+import JobDetails from './pages/JobDetails'
+
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/sign-up" element={<SignupPage />} />
         <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="/jobs" element={<AllJobs />} />
+
+        <Route path="/jobs/:id" element={<JobDetails />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
