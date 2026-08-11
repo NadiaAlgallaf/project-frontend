9cc32385df68a46b593dae8d26173b4c1dea7887
Nadia Algallaf <nadiaalgallaf164@gmail.com>
2026-08-11 18:45:04 +0300

    add function to prevent the employer from selecting past date and time

--- src/pages/Application/ViewApplications.jsx
+++ src/pages/Application/ViewApplications.jsx
@@ -38,12 +38,28 @@ function ViewApplications() {
   }
 
   async function handleInterviewDate(applicationId) {
+    if (new Date(interviewDate) < new Date()) {
+      alert('Please choose a future date and time')
+      return
+    }
     try {
       await updateInterviewDate(applicationId, interviewDate)
       alert('Interview date saved')
     } catch (error) {
       console.log(error)
     }
+  }
+
+  function getCurrentDateTime() {
+    const now = new Date()
+
+    const year = now.getFullYear()
+    const month = String(now.getMonth() + 1).padStart(2, '0')
+    const day = String(now.getDate()).padStart(2, '0')
+    const hours = String(now.getHours()).padStart(2, '0')
+    const minutes = String(now.getMinutes()).padStart(2, '0')
+
+    return `${year}-${month}-${day}T${hours}:${minutes}`
   }
 
   useEffect(() => {
@@ -89,6 +105,8 @@ function ViewApplications() {
 
                 <input
                   type="datetime-local"
+                  min={getCurrentDateTime()}
+                  value={interviewDate}
                   onChange={(event) => setInterviewDate(event.target.value)}
                 />
 
